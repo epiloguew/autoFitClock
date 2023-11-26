2cadec0dd7f126025ced3b7e11c333ebfe83ca4e
epilogue <125470119+epiloguew@users.noreply.github.com>
2023-11-27 06:15:43 +0800

    feat：获取当前时间

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,6 +1,24 @@
-<script setup></script>
+<script setup>
+import { format } from "silly-datetime";
+import { ref, onMounted, onBeforeUnmount } from "vue";
+let count = ref("");
+let timer = ref(null);
+onMounted(() => {
+  timer.value = setInterval(() => {
+    count.value = fn();
+  }, 1000);
+});
+onBeforeUnmount(() => {
+  clearInterval(timer.value);
+});
+const fn = () => {
+  return format(new Date(), "YYYY-MM-DD HH:mm:ss");
+};
+</script>
 
-<template></template>
+<template>
+  {{ count }}
+</template>
 
 <style lang="less">
 @import "./assets/css/styles.less";
