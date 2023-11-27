8263bc85df430225afcdb656565688201631d7ee
epilogue <125470119+epiloguew@users.noreply.github.com>
2023-11-28 06:29:25 +0800

    feat:时间展示修改

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,28 +1,50 @@
 <script setup>
 import { format, fromNow } from "silly-datetime";
-import { ref, onMounted, onBeforeUnmount } from "vue";
+import { ref, watch, onMounted, onBeforeUnmount } from "vue";
 
 let count = ref("");
+let fixTime = ref("");
 let timer = ref(null);
+const appoint = "08:00:00";
+const origin = "14:00:00";
 onMounted(() => {
   timer.value = setInterval(() => {
     count.value = fn();
   }, 1000);
-  console.log(format(new Date(), "HH:mm"));
 });
 onBeforeUnmount(() => {
   clearInterval(timer.value);
 });
-const fn = () => {
-  return format(new Date(), "YYYY-MM-DD HH:mm:ss");
+watch(count, () => {
+  fixTime.value = getFixTime();
+});
+const fn = (day) => {
+  const str = "YYYY-MM-DD";
+  const option = day ? str : str + " HH:mm:ss";
+  return format(new Date(), option);
+};
+const getFixTime = () => {
+  const [appointHour] = appoint.split(":");
+  const [originHour] = origin.split(":");
+  if (originHour <= appointHour) return fn();
+  const diff = originHour - appointHour;
+  const fixHour = format(new Date(), "HH") - diff;
+  const strFix = fixHour < 10 ? "0" + fixHour : fixHour.toString();
+  return fn(true) + " " + strFix + ":" + format(new Date(), "mm:ss");
 };
 </script>
 
 <template>
   <div style="">
+    <div class="top">预定时间: {{ fn(true) }} {{ appoint }}</div>
+    <div class="top">起始时间: {{ fn(true) }} {{ origin }}</div>
     <div class="top">
       当前时间:
       {{ count }}
+    </div>
+    <div class="top">
+      修正时间:
+      {{ fixTime }}
     </div>
   </div>
 </template>
---
+++ src/renderer/src/components/timer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="top">
+    当前时间:
+    {{ count }}
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="less" scoped>
+.top {
+  color: rgb(54, 207, 204);
+  font-size: 20px;
+}
+</style>
